10b40f8e90f59b0cd123d91ba9dfe3d79e5e0b28
Zerin Nichols <113934852+NicholsTechnology@users.noreply.github.com>
2022-12-13 12:59:44 -0600

    Update login-page.js

--- login/login-page.js
+++ login/login-page.js
@@ -8,8 +8,8 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "user" && password === "web_dev") {
-        alert("You have successfully logged in.");
+    if (username === "ZerinNichols" && password === "467251zn") {
+        alert("You have successfully logged in as ZerinNichols.");
         location.reload();
     } else {
         loginErrorMsg.style.opacity = 1;
