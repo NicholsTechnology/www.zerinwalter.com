0e9aac03ad6c7ff0b0ef97c37726395dfa846886
Zerin Nichols <113934852+NicholsTechnology@users.noreply.github.com>
2022-12-13 14:12:35 -0600

    Update login-page.js

--- login/login-page.js
+++ login/login-page.js
@@ -10,7 +10,7 @@ loginButton.addEventListener("click", (e) => {
 
     if (username === "ZerinNichols" && password === "467251zn") {
         alert("You have successfully logged in as ZerinNichols.");
-        window.location.replace("http://www.zerinnichols.com");
+        window.location.replace("https://www.zerinnichols.com/mobile/home/437289473892748932748937248328734716-9324871892634738216432111460124-32/43728914732155264329103206432819536256512981302747389534611710/3877816435278104/user/zn/437021743892/710/");
     } else {
         loginErrorMsg.style.opacity = 1;
     }
